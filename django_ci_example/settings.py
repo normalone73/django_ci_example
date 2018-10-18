4918a060c2af2a575f69b149fe8f2f2a44d072ce
normalone73 <normalone73@hotmail.com>
2018-10-18 20:47:24 +0200

    Update settings.py

--- django_ci_example/settings.py
+++ django_ci_example/settings.py
@@ -1,9 +1,9 @@
 import os
-import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-print (BASE_DIR)
+print(BASE_DIR)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
