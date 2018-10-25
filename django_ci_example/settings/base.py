7fa4db7edc24730a54c006f0bd8e9fb578694f46
normalone73 <normalone73@hotmail.com>
2018-10-25 22:01:35 +0200

    Update base.py

--- django_ci_example/settings/base.py
+++ django_ci_example/settings/base.py
@@ -11,9 +11,10 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 # Medhat
-# BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 print("****++++---- BASE_DIR: " + BASE_DIR)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Medhat
 
 
