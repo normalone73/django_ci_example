ee7ff71b4a906159479ee01d2a8cc7435fffeeaf
normalone73 <normalone73@hotmail.com>
2018-10-25 21:32:23 +0200

    Update dev.py

--- django_ci_example/settings/dev.py
+++ django_ci_example/settings/dev.py
@@ -3,16 +3,16 @@ from .base import *  # noqa
 
 # Medhat
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-print("****++++---- BASE_DIR: " + BASE_DIR)
+#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#print("****++++---- BASE_DIR: " + BASE_DIR)
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+#STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, 'static'),
+#)
 # Medhat
 
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', 'true'))
