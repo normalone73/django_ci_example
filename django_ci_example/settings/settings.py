51bdcc5a65de0820bce8b2ebec4bfdcc08a2654c
normalone73 <normalone73@hotmail.com>
2018-10-25 21:08:46 +0200

    Create settings.py

---
+++ django_ci_example/settings/settings.py
@@ -0,0 +1,16 @@
+import os
+
+# Touch 1
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+print("****++++---- BASE_DIR: " + BASE_DIR)
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
