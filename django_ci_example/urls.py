836300075b0f9b9dc5a39b0a472b3861529c2b51
normalone73 <normalone73@hotmail.com>
2018-10-26 16:26:17 +0200

    Update urls.py

--- django_ci_example/urls.py
+++ django_ci_example/urls.py
@@ -3,13 +3,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-# Medhat
-# urlpatterns = patterns('',
-# url(r'^polls/', include('polls.urls', namespace="polls")),
-# url(r'^admin/', include(admin.site.urls)),
-# )
-urlpatterns = [
+urlpatterns = patterns('',
     url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
-]
-# Medhat
+)
